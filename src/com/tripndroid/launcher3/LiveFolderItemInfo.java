dba9b7f558b8eeb37a94ff250b152f84c81e2ca4
TripNRaVeR <noerialexeew@live.nl>
2013-09-15 03:56:54 +0200

    initial commit

---
+++ src/com/tripndroid/launcher3/LiveFolderItemInfo.java
@@ -0,0 +1,12 @@
+package com.tripndroid.launcher3;
+
+
+public class LiveFolderItemInfo extends ShortcutInfo {
+
+    int item_id;
+
+    LiveFolderItemInfo() {
+        super();
+        requiresDbUpdate = false;
+    }
+}
